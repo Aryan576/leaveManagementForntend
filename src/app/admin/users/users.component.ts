4bfe9dc7438d42a8337dda7b91e5e0eade53c818
Aryan576 <aryanshah576@gmail.com>
2023-03-11 16:36:35 +0530

    Add users

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AdminserviceService } from '../adminservice.service';
 
 @Component({
   selector: 'app-users',
@@ -7,10 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
-
-  constructor() { }
+  adminusers:any={}
+  constructor(private rut:Router,private toastr: ToastrService,
+    private admin: AdminserviceService) { }
   
   ngOnInit(): void {
+    
+    this.admin.getadminUsers().then(users=>{
+        this.adminusers=users.results
+    })
+    
     this.dtOptions = {
       pagingType: 'full_numbers'
     };
